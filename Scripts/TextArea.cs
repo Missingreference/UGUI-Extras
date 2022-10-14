48674271750d9aa4046c73f7e6ef2f6aa1e507f8
Missingreference <Missingreference@users.noreply.github.com>
2022-10-14 12:46:17 -0400

    Actual TextArea Spacing Fix

--- Scripts/TextArea.cs
+++ Scripts/TextArea.cs
@@ -882,7 +882,7 @@ namespace Elanetic.UI.Unity
 
                 TextMeshProUGUI textMesh = m_VisibleLines[i];
                 textMesh.SetCharArray(m_RawText, lineStartIndex, lineSize);
-                textMesh.rectTransform.anchoredPosition = new Vector2(0, -fontSize * i);
+                textMesh.rectTransform.anchoredPosition = new Vector2(1.0f, -fontSize * i);
 
                 if (lineSize <= 0) continue; //Without this check calling textMesh.UpdateVertexData pushes old data to the shader
 
